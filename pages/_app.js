a3520fbc230afe19ab53f8ef2d294ed8046764d3
kodai hiradate <koudai.hiradate@gmail.com>
2025-03-26 13:51:03 +0900

    add meta tags and favicon to improve SEO and branding

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { Box, ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 import Navigation from '../components/Navigation';
 import usePostHog from '../hooks/usePostHog';
 import theme from '../styles/theme';
@@ -9,6 +10,19 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>App Release Note Generator</title>
+        <meta name="description" content="アプリのリリースノートを複数言語で簡単に生成" />
+        <meta property="og:title" content="App Release Note Generator" />
+        <meta property="og:description" content="アプリのリリースノートを複数言語で簡単に生成" />
+        <meta property="og:image" content="/assets/ogp.png" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="App Release Note Generator" />
+        <meta name="twitter:description" content="アプリのリリースノートを複数言語で簡単に生成" />
+        <meta name="twitter:image" content="/assets/ogp.png" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navigation />
       <Box pt="60px">
         <Component {...pageProps} />
