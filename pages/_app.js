056f4038ce71a9bb4e6e6088da9c230207026f5d
kodai hiradate <koudai.hiradate@gmail.com>
2025-03-27 10:15:49 +0900

    update Open Graph and Twitter meta tags with absolute URLs for improved sharing

--- pages/_app.js
+++ pages/_app.js
@@ -13,18 +13,14 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>App Release Note Generator</title>
         <meta name="description" content="アプリのリリースノートを複数言語で簡単に生成" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@hiradaty" />
-        <meta name="twitter:title" content="アプリのリリースノートを複数言語で簡単に生成" />
-        <meta name="twitter:image" content="/assets/ogp.png" />
         <meta property="og:title" content="App Release Note Generator" />
         <meta property="og:description" content="アプリのリリースノートを複数言語で簡単に生成" />
-        <meta property="og:image" content="/assets/ogp.png" />
+        <meta property="og:image" content="https://releasenote-generator.netlify.app/assets/ogp.png" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="App Release Note Generator" />
         <meta name="twitter:description" content="アプリのリリースノートを複数言語で簡単に生成" />
-        <meta name="twitter:image" content="/assets/ogp.png" />
+        <meta name="twitter:image" content="https://releasenote-generator.netlify.app/assets/ogp.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navigation />
